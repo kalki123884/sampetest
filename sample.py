615efe7d0b7220e78d80c8f95b023ce30179742d
EC2 Default User <ec2-user@ip-172-31-95-82.ec2.internal>
2025-08-01 02:10:13 +0000

    added sample python script

---
+++ sample.py
@@ -0,0 +1,16 @@
+#
+#
+#
+#
+#
+#
+#
+#
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+@app.get("/")
+def test1():
+    return "testing"
+if __name__ == "__main__":
+    uvicorn.run("sample:app",host="0.0.0.0",port=8000,reload=True)
