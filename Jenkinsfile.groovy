5403f4eb40746545c7a7d4db744dcf64d6d94ef3
sankar inkollu <sivasankar.inkollu@yahoo.com>
2025-09-18 05:17:00 +0000

    modify jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,46 +11,61 @@ node {
     env.nameprefix = "siva"
     
     stage("Checkout") {
-        git branch: 'main', url: 'https://github.com/kalki123884/sampetest.git'
+        scmVars = checkout(scm)
     }
 
     stage("Install dependencies"){
-        echo "Installing Python dependencies"
-        sh 'pip install --upgrade fastapi'
-        sh 'pip install --upgrade uvicorn'
-        
+        echo "Installing Python dependencies" 
     }
-    #stage("Sonarqube"){
-    #    withSonarQubeEnv('SonarQube Cloud') {
-    #          def scannerHome = tool 'sampetest-sonar'
-    #           sh """
-    #            ${scannerHome}/bin/sonar-scanner \
-    #            -Dsonar.projectKey=sampetest \
-    #            -Dsonar.sources=. \
-    #            -Dsonar.organization=kalki123884
-    #        """
-    #    }
-    #}
-    stage("prepare image"){
-        echo "Deploying application"
-        sh 'docker build -t sample:v1 .'
+    stage("prepare"){
+      echo "${scmVars}"
+      imagetag = "${scmVars.GIT_COMMIT}".take(7)
+      fullimage  = "sivasankarinkollu1/sample:${imagetag}"
+      echo "${image}" 
     }
-    stage("Stop and remove app container"){
-        echo "Removing container"
-        sh '''
-           #!/bin/bash
-           set -e
-           if docker ps | grep -i sampleapp; then
-              docker stop sampleapp && docker rm sampleapp
-           elif docker ps -a | grep -i sampleapp; then
-              docker rm sampleapp
-           else
-              echo "no sample app found to remove"
-           fi
-        '''
+    //stage("Sonarqube"){
+    //    withSonarQubeEnv('SonarQube Cloud') {
+    //          def scannerHome = tool 'sampetest-sonar'
+    //           sh """
+    //            ${scannerHome}/bin/sonar-scanner \
+    //            -Dsonar.projectKey=sampetest \
+    //            -Dsonar.sources=. \
+    //            -Dsonar.organization=kalki123884
+    //        """
+    //    }
+    //}
+    stage("prepare docker image"){
+      docker.build(fullimage)
     }
-    stage("Deploy application"){
-        echo "Deploy sample app"
-        sh 'docker run -d --name sampleapp -p 8000:8000 sample:v1'
+    stage("Lint") {
+      echo "Performing Lint check"
     }
+    stage("push image"){
+      echo "push image to repo"
+      docker.withRegistry("https://index.docker.io/v1/","siva-docker-login"){
+        image.push(fullimage)
+      }
+    }
+    //stage("prepare image"){
+    //    echo "Deploying application"
+    //    sh 'docker build -t sample:v1 .'
+    //}
+    //stage("Stop and remove app container"){
+    //    echo "Removing container"
+    //    sh '''
+    //       #!/bin/bash
+    //       set -e
+    //       if docker ps | grep -i sampleapp; then
+    //          docker stop sampleapp && docker rm sampleapp
+    //       elif docker ps -a | grep -i sampleapp; then
+    //          docker rm sampleapp
+    //       else
+    //          echo "no sample app found to remove"
+    //       fi
+    //    '''
+    //}
+    //stage("Deploy application"){
+    //    echo "Deploy sample app"
+    //    sh 'docker run -d --name sampleapp -p 8000:8000 sample:v1'
+    //}
 }
