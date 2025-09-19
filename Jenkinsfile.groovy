599a2c11b08b7e57d314578d508a046f1ff59411
sankar inkollu <sivasankar.inkollu@yahoo.com>
2025-09-19 04:34:23 +0000

    added deploy step

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -54,26 +54,8 @@ node {
         image.push("latest")
       }
     }
-    //stage("prepare image"){
-    //    echo "Deploying application"
-    //    sh 'docker build -t sample:v1 .'
-    //}
-    //stage("Stop and remove app container"){
-    //    echo "Removing container"
-    //    sh '''
-    //       #!/bin/bash
-    //       set -e
-    //       if docker ps | grep -i sampleapp; then
-    //          docker stop sampleapp && docker rm sampleapp
-    //       elif docker ps -a | grep -i sampleapp; then
-    //          docker rm sampleapp
-    //       else
-    //          echo "no sample app found to remove"
-    //       fi
-    //    '''
-    //}
-    //stage("Deploy application"){
-    //    echo "Deploy sample app"
-    //    sh 'docker run -d --name sampleapp -p 8000:8000 sample:v1'
-    //}
+    stage("deploy application in k8s cluster"){
+      sh 'kubectl get po'
+      helm list
+    }
 }
